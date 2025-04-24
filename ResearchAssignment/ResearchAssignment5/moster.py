456d59b2bb0c947a1b97ab323ebcd944583ac518
Swapnaneel Dey <ourba.de@gmail.com>
2025-04-23 19:40:12 -0700

    New class

--- ResearchAssignment/ResearchAssignment5/moster.py
+++ ResearchAssignment/ResearchAssignment5/moster.py
@@ -96,5 +96,64 @@ class AbundanceMatching:
         starMass = self.mhalo*self.SHMratio()
 
         return starMass
+    
+    def error(self):
+        """Method to compute the error in the stellar mass
+        using eq 2 of Moster+2013 (stellar / halo mass ratio)
+        
+        Output;
+            starMass: float, stellar mass in Msun"""
+        M1_up = 10**(11.59+0.236) # Converting characteristic mass 
+        # to Msun from Log(Msun)
+        M1_low = 10**(11.59-0.236) # Converting characteristic mass
+        A = (self.mhalo/M1_up)**(-(1.376+0.153))  # Low mass end
+        
+        B = (self.mhalo/M1_low)**(0.608+0.059)   # High mass end
+        
+        Norm = 2*(0.0351+0.0058) # Normalization
+        print(Norm*(A+B)**(-1))
+        sm_upperlimit = self.mhalo*Norm*(A+B)**(-1)
 
+        M1 = 10**(11.59-0.236) # Converting characteristic mass 
+        # to Msun from Log(Msun)
+        
+        A = (self.mhalo/M1_low)**(-(1.376-0.153))  # Low mass end
+        
+        B = (self.mhalo/M1_up)**(0.608-0.059)   # High mass end
+        
+        Norm = 2*(0.0351-0.0058) # Normalization
+        print(Norm*(A+B)**(-1))
+        sm_lower_lim = self.mhalo*Norm*(A+B)**(-1)
 
+        return sm_upperlimit, sm_lower_lim
+
+    def compute_sigma_m(self, unc_N = 0.0058, unc_M1 = 0.236, unc_beta = 0.153, unc_gamma = 0.059):
+        M = self.mhalo                          # Halo mass
+        logM1 = self.logM1()                    # Log base 10 of characteristic mass
+        M1 = 10**logM1                          # Characteristic mass
+        N = self.N()                            # Normalization factor
+        beta = self.Beta()                      # Slope at low mass end
+        gamma = self.Gamma()                    # Slope at high mass end
+
+        x = M / M1
+        denom = (x**(-beta) + x**gamma)
+        
+        # Partial derivatives
+        dm_dN = (2 * M) / denom
+
+        dm_dlogM1 = 2 * M * N * np.log(10) * (gamma * x**gamma - beta * x**(-beta)) / denom**2
+
+        dm_dbeta = 2 * M * N * (x**(-beta) * np.log(x)) / denom**2
+
+        dm_dgamma = -2 * M * N * (x**gamma * np.log(x)) / denom**2
+
+        # Total variance from each parameter's uncertainty
+        sigma_m_squared = (
+            (dm_dN * unc_N)**2 +
+            (dm_dlogM1 * unc_M1)**2 +  # Uncertainty in M1 is already in log10 units
+            (dm_dbeta * unc_beta)**2 +
+            (dm_dgamma * unc_gamma)**2
+        )
+
+        sigma_m = np.sqrt(sigma_m_squared)
+        return sigma_m
